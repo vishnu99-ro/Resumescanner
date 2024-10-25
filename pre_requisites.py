d552eda42c6ec36d2f02c91131b6d68d8d575739
vishnu99-ro <vishnuvardhan99490@gmail.com>
2024-10-25 21:30:42 +0530

    Update and rename pre_requisites.py to style.css

--- pre_requisites.py
+++
@@ -1,7 +0,0 @@
-import os
-
-# Install SpaCy Dependencies
-os.system('python3 -m spacy download en_core_web_lg')
-os.system('python3 -m spacy download en_core_web_sm')
-
-# Install nltk Dependencies
